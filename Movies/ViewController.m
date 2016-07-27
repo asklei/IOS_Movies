df1ba31a15ea224f76f1928dcc4ff5af03a46ed3
Lei Xu <leix@uber.com>
2016-07-27 00:21:42 -0700

    Set up basic view and search

--- Movies/ViewController.m
+++ Movies/ViewController.m
@@ -7,21 +7,69 @@
 //
 
 #import "ViewController.h"
+#import "Masonry.h"
 
 @interface ViewController ()
-
+@property(nonatomic, weak) UISearchBar *search;
+@property(nonatomic, weak) UITextView *content;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    UISearchBar *search = [UISearchBar new];
+    UITextView *content = [UITextView new];
+    self.search = search;
+    self.content = content;
+    [self.view addSubview:search];
+    [self.view addSubview:content];
+    
+    
+    search.placeholder = @"search";
+    search.searchBarStyle = UISearchBarStyleMinimal;
+    content.editable = false;
+    content.layer.borderColor = [[UIColor colorWithRed:204.0/255.0 green:204.0/255.0 blue:204.0/255.0 alpha:1.0] CGColor];
+    content.layer.cornerRadius = 5.0f;
+    content.layer.borderWidth = 0.5f;
+    
+    self.search.delegate = self;
+    
+    int spaceD = 10;
+    
+    [search mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.mas_topLayoutGuide).offset(spaceD);
+        make.leading.equalTo(self.view.mas_leading).offset(spaceD);
+        make.trailing.equalTo(self.view.mas_trailing).offset(-spaceD);
+    }];
+    
+    [content mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.search.mas_bottom).offset(spaceD*2);
+        make.leading.equalTo(self.search.mas_leading);
+        make.trailing.equalTo(self.search.mas_trailing);
+        make.bottom.equalTo(self.view.mas_bottom).offset(-spaceD*2);
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:YES];
+}
+
+# pragma mark: UISearchBarDelegate
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    [searchBar resignFirstResponder];
+}
+
+- (void)doSearch {
+    
 }
 
 @end
