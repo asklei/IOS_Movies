92e1c27684fb6c91c66894eac0a94d640be766cf
Lei Xu <leix@uber.com>
2016-07-26 22:49:17 -0700

    Initial Commit

---
+++ Movies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Movies
+//
+//  Created by Lei Xu on 7/26/16.
+//  Copyright © 2016 Uber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
